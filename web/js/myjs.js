8e010e5c6ecb77e69fc6299a36cb51c6d7185df9
Dynasty <Dynasty@DESKTOP-COITQ39>
2019-01-30 12:58:15 +0100

    Commit Inicio

---
+++ web/js/myjs.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    console.log('ready');
+    //init();
+});
+
+//function init(){
+	// ....
+    // Listener
+    //onclicksave();
+//}
+
+//var onclicksave = function(){	
+//       $("#save").click(function (e){
+//                console.log('savecat');
+//        });
+//}
